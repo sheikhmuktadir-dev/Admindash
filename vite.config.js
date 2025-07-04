f7c486dc2123dcbfd2c5fe83a6f8e15d5a448e10
sheikhmuktadir-dev <sheikh@bizionictech.com>
2025-07-04 17:46:45 +0530

    fix: update paths and config for GitHub Pages deployment vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import history from "connect-history-api-fallback";
 
 export default defineConfig({
-  base: "/your-repo-name/", // 🔁 Replace with your GitHub repo name
+  base: "/Admindash/", // 🔁 Replace with your GitHub repo name
   plugins: [
     {
       name: "spa-fallback",
